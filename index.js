b91bcb059e86bca88e006bdf2770da400596a45c
Celio Costa <celiorscosta@hotmail.com>
2022-08-27 01:05:05 -0300

    regex 01

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ import chalk from 'chalk';
 import fs  from 'fs';
 import { encode } from 'punycode';
 
+const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
+
+function extraiLinks(texto){
+    const regex =/\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const linksExtraidos = texto.match(regex);
+    console.log(linksExtraidos);
+
+
+}
+
 function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'Não há arquivo'));
 }
@@ -16,23 +26,5 @@ async function pegaArquivo(caminhoDoArquivo){
         trataErro(erro);
     }
 }
-
-// function pegaArquivo(caminhoDoArquivo){
-//     const encoding = 'utf-8';
-//     fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((text)=> console.log(chalk.green(text)))
-//     .catch((erro)=> trataErro(erro));
-// }
-
-// function pegaArquivo(caminhoDoArquivo){
-//     const encoding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=>{
-//         if(erro){
-//             trataErro(erro);
-//         }
-//         console.log(chalk.green(texto));
-//     });
-// }
-
-pegaArquivo('./arquivos/texto1.md');
+extraiLinks(texto);
+// pegaArquivo('./arquivos/texto1.md');
