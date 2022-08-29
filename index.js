edbce6ed225d92345ada82d98c9b796cb755bdcc
celiorscosta <celiorscosta@hotmail.com>
2022-08-29 12:32:10 -0300

    Funcionando.

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-import chalk from 'chalk';
-import { timeEnd } from 'console';
-import fs from 'fs';
-
+const chalk = require('chalk');
+const fs = require('fs');
 
 function extraiLinks(texto) {
-    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-    const arrayResultados = [];
-    let temp;
-
-    while ((temp = regex.exec(texto)) != null) {
-        arrayResultados.push({ [temp[1]]: temp[2] });
-
-    }
-    return arrayResultados;
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] })
+  }
+  return arrayResultados.length === 0 ? 'não há links' : arrayResultados;
 }
 
 function trataErro(erro) {
-    throw new Error(chalk.red(erro.code, 'Não há arquivo'));
+  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    try {
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(extraiLinks(texto));
-    }
-    catch (erro) {
-        trataErro(erro);
-    }
+  const encoding = 'utf-8';
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    return extraiLinks(texto);
+  } catch(erro) {
+    trataErro(erro);
+  }
 }
-//pegaArquivo('./arquivos/texto1.md');
- module.exports = pegaArquivo;
+
+module.exports = pegaArquivo;
