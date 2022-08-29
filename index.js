6f9de3b2be22632967f18356447c2fbae954f55a
celiorscosta <celiorscosta@hotmail.com>
2022-08-29 10:04:14 -0300

    Atualização com aula 04

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 import chalk from 'chalk';
-import fs  from 'fs';
-import { encode } from 'punycode';
-
-const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
-
-function extraiLinks(texto){
-    const regex =/\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-    const linksExtraidos = texto.match(regex);
-    console.log(linksExtraidos);
+import { timeEnd } from 'console';
+import fs from 'fs';
 
 
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+
+    while ((temp = regex.exec(texto)) != null) {
+        arrayResultados.push({ [temp[1]]: temp[2] });
+
+    }
+    return arrayResultados;
 }
 
-function trataErro(erro){
+function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo'));
 }
 
-async function pegaArquivo(caminhoDoArquivo){
-    try{   
-        const encoding = 'utf-8';
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    try {
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.green(texto));
+        console.log(extraiLinks(texto));
     }
-    catch(erro){
+    catch (erro) {
         trataErro(erro);
     }
 }
-extraiLinks(texto);
-// pegaArquivo('./arquivos/texto1.md');
+pegaArquivo('./arquivos/texto1.md');
